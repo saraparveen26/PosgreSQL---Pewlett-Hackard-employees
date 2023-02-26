6c35693f1d2d7b2f14dc21030eb0e2da6dd5f5d7
saraparveen26 <sara.parveen26@gmail.com>
2023-02-26 18:10:38 -0500

    tables created and csv imported

--- Employees_SQL/employees_schema.sql
+++ Employees_SQL/employees_schema.sql
@@ -7,6 +7,12 @@ DROP TABLE IF EXISTS Employees;
 DROP TABLE IF EXISTS Titles;
 
 
+-- Change 'date' format to MDY for the database I am using: 'employees_db'
+-- The CSV named 'employees' has dates in MM-DD-YYYY format
+-- PostgreSQL defaults to YYYY-MM-DD so altering this setting
+ALTER DATABASE employees_db SET datestyle TO "ISO, MDY";
+	
+-- Create all tables
 -- Create a table of titles
 CREATE TABLE Titles(
 	title_id VARCHAR(10) NOT NULL,
@@ -15,21 +21,19 @@ CREATE TABLE Titles(
 	PRIMARY KEY (title_id)
 );
 
-
 -- Create a table of employees
 CREATE TABLE Employees(
 	employee_no INTEGER NOT NULL,
 	employee_title_id VARCHAR(10) NOT NULL,
 	FOREIGN KEY (employee_title_id) REFERENCES Titles(title_id),
+	birth_date DATE NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	gender VARCHAR(10) NOT NULL,
-	birth_date DATE NOT NULL,
 	hire_date DATE NOT NULL,
 	UNIQUE (employee_no),
 	PRIMARY KEY (employee_no)
 );
-
 
 -- Create a table of departments
 CREATE TABLE Departments(
@@ -39,28 +43,23 @@ CREATE TABLE Departments(
 	PRIMARY KEY (dept_no)
 );
 
-
 -- Create a table of department employees
 CREATE TABLE Dept_Employees(
 	employee_no INTEGER NOT NULL,
 	FOREIGN KEY (employee_no) REFERENCES Employees(employee_no),
 	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
-	UNIQUE (employee_no),
-	PRIMARY KEY (employee_no)
+	PRIMARY KEY (employee_no, dept_no)
 );
-
 
 -- Create a table of department managers
 CREATE TABLE Dept_Managers(
-	employee_no INTEGER NOT NULL,
-	FOREIGN KEY (employee_no) REFERENCES Employees(employee_no),
 	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
-	UNIQUE (employee_no),
-	PRIMARY KEY (employee_no)
+	employee_no INTEGER NOT NULL,
+	FOREIGN KEY (employee_no) REFERENCES Employees(employee_no),
+	PRIMARY KEY (dept_no, employee_no)
 );
-
 
 -- Create a table of salaries
 CREATE TABLE Salaries(
@@ -70,3 +69,13 @@ CREATE TABLE Salaries(
 	UNIQUE (employee_no),
 	PRIMARY KEY (employee_no)
 );
+
+
+-- Import each CSV file into its corresponding SQL table
+-- Display each table to confirm data is imported properly
+SELECT * FROM Titles;
+SELECT * FROM Employees;
+SELECT * FROM Departments;
+SELECT * FROM Dept_Employees;
+SELECT * FROM Dept_Managers;
+SELECT * FROM Salaries;
